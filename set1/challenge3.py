e8d674f2d65ba5fc2e106b516ecd267ce7dcc997
varadgunjal <varadgunjal@users.noreply.github.com>
2016-09-22 10:01:55 +0530

    Update challenge3.py

--- set1/challenge3.py
+++ set1/challenge3.py
@@ -50,13 +50,13 @@ def single_byte_xor_for_multi_byte_key(most_common_bytes):
     for password in byte_passwords:
         # For each byte in most_common_bytes, xor with a candidate byte 
         # password.  Convert the output bytes to ascii / utf-8 and verify how
-        # many, if any, match with ETAOINSHRDLU[space]
+        # many, if any, match with ETAOIN[space]SHRDLU
 
         chars_match = 0
 
         for cipher_byte in most_common_bytes:
             character = chr(int(cipher_byte, 16) ^ int(password, 16))
-            if character in "etaoinshrdlu ":
+            if character in "etaoin shrdlu":
                 chars_match += 1
         
         if chars_match > max_matching_chars:
